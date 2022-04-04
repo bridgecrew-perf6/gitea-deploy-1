8b965d1646657ec6b337dd3f9e154c82e05619aa
vyannis <yannis.verhasselt@reseau.eseo.fr>
2022-04-04 11:59:09 +0200

    user commands changes for backup and restore

--- files/backup_gitea.sh
+++ files/backup_gitea.sh
@@ -4,17 +4,16 @@
 # we will use the command line method given in Gitea's documentation
 # https://docs.gitea.io/en-us/backup-and-restore/
 
+# Switch to git user
+sudo -H -u git bash -c '
 REMOTE_SERVER_LOCATION="etudiant@192.168.0.42:/Data/etudiant/Backup-gitea/"
 LOG_FILE_BACKUP="/home/git/logs/backup.log"
 SSH_CRED="/home/git/.ssh/giteakey"
 DATE="`date +"%H%M%d%m%Y"`"
 
-# Switch to git user
-sudo -H -u git bash -c '
-cd /home/git/
-gitea dump -w /home/git/dump/  -c /etc/gitea/app.ini -f /home/git/backup-gitea/$DATE.zip >> $LOG_FILE_BACKUP 2>&1
+gitea dump -w /home/git/dump/  -c /etc/gitea/app.ini -f /home/git/backup-gitea/$DATE.zip >> $LOG_FILE_BACKUP 2>&1 
 
 # Sends the backed up zip to remote server
-scp -q -o "StrictHostKeyChecking=no" -i $SSH_CRED /home/git/backup-gitea/$DATE.zip $REMOTE_SERVER_LOCATION
+scp -o "StrictHostKeyChecking=no" -i $SSH_CRED /home/git/backup-gitea/$DATE.zip $REMOTE_SERVER_LOCATION
 
 '
--- files/restore_gitea.sh
+++ files/restore_gitea.sh
@@ -4,6 +4,11 @@
 # we will use the command line method given in Gitea's documentation
 # https://docs.gitea.io/en-us/backup-and-restore/
 
+
+sudo rm -rf /etc/gitea/app.ini /var/lib/gitea/data/* /var/lib/gitea/log/* /var/lib/gitea/gitea-repositories/*
+
+sudo -H -u git bash -c '
+
 # Backup location
 REMOTE_SERVER="etudiant@192.168.0.42"
 # ID is the value of the server backup to be restored
@@ -16,25 +21,21 @@ USER="git"
 PASS="secret"
 DATABASE="gitea"
 
-sudo rm -rf /etc/gitea/app.ini /var/lib/gitea/data/* /var/lib/gitea/log/* /var/lib/gitea/gitea-repositories/*
-
-sudo -H -u git bash -c '
-
 if ssh -q -o "StrictHostKeyChecking=no" -i $SSH_CRED $REMOTE_SERVER test -e $BACKUP; then
     # Collects the backup from the remote server
     echo "=> [1]: Collecting backup from remote server..."
     rm -rf /home/git/backup-recovery
     mkdir /home/git/backup-recovery && cd /home/git/backup-recovery
-    scp -q -i $SSH_CRED $REMOTE_BACKUP /home/git/backup-recovery/
+    scp -q -i $SSH_CRED $REMOTE_BACKUP /home/git/backup-recovery/ 
     echo "END - Collecting backup from remote server..."
-    unzip $ID.zip
+    unzip $ID.zip ;
     echo "=> [2]: Restoring backup..."
     mv -f app.ini /etc/gitea/app.ini
     mv -f data/* /var/lib/gitea/data/
     mv -f log/* /var/lib/gitea/log/
     mv -f repos/* /var/lib/gitea/gitea-repositories/
     mysql --default-character-set=utf8mb4 -u$USER -p$PASS $DATABASE <gitea-db.sql
-    chown -R git:git /etc/gitea/app.ini /var/lib/gitea 
+    chown -R git:git /etc/gitea/app.ini /var/lib/gitea
     
     
     echo "END - Backup restored"
